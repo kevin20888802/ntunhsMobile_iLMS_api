6eecc4fcd22d060f451f486a0f9fae5cfea234c6
kevin20888802 <kevin20888802@gmail.com>
2021-12-19 15:05:48 +0800

    0.0.0.0

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ app.register_blueprint(ilms_recentwork)
 
 if __name__ == "__main__":
     if local_test == True:
-        app.run(host='127.0.0.1',port=5001)
+        app.run(host='0.0.0.0',port=5001)
     else:
         app.run()
     pass
